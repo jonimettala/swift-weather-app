3b7c25c45d460b182720d7bbb3120fa4347d950e
Joni Mettälä <joni.mettala@gmail.com>
2018-10-03 16:31:20 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Joni Mettälä on 03/10/2018.
+//  Copyright © 2018 Joni Mettälä. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
