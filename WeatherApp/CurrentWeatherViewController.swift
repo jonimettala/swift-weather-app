246bd3f546b9e7ea052bcc44d068faf4a8b72dad
Joni Mettälä <joni.mettala@gmail.com>
2018-10-06 15:42:25 +0300

    Add ViewController file for the current weather

---
+++ WeatherApp/CurrentWeatherViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CurrentWeatherViewController.swift
+//  WeatherApp
+//
+//  Created by Joni Mettälä on 06/10/2018.
+//  Copyright © 2018 Joni Mettälä. All rights reserved.
+//
+
+import UIKit
+
+class CurrentWeatherViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
